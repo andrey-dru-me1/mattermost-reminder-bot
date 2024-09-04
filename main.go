c8870a7d5ab3a89b6f6c3c442d45d9507bd15e7b
andrey-dru-me1 <andrey260503@gmail.com>
2024-09-04 16:19:57 +0700

    Replace hardcoded database connection with env-configurable one

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
@@ -62,16 +62,18 @@ func main() {
 	router.POST("/mattermost/reminders", mattermostReminder)
 
 	router.Run(":8080")
-	// router.Run("localhost:8080")
 }
 
 func setupDatabase() (*sql.DB, error) {
+	cfg := mysql.Config{
+		User:   os.Getenv("MYSQL_USER"),
+		Passwd: os.Getenv("MYSQL_PASSWORD"),
+		Net:    "tcp",
+		Addr:   fmt.Sprintf("%s:%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT")),
+		DBName: os.Getenv("DB_NAME"),
+	}
 
-	dbPassword := os.Getenv("MYSQL_PASSWORD")
-
-	dbUrl := fmt.Sprintf("reminders:%s@tcp(localhost:3306)/reminders", dbPassword)
-
-	db, err := sql.Open("mysql", dbUrl)
+	db, err := sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		return nil, err
 	}
